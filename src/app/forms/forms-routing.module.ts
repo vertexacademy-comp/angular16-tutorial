96afcf04d0a3102a243cca09f7f4fcefee18f03e
Prashant <vacationlocation.devteam@gmail.com>
2024-12-23 18:18:08 +0530

    Reactive forms

--- src/app/forms/forms-routing.module.ts
+++ src/app/forms/forms-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TemplateFormsComponent } from './template-forms/template-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'templateforms', pathMatch:'full'},
   {path:'templateforms', component: TemplateFormsComponent},
+  {path:'reactiveforms', component: ReactiveFormsComponent},
+
 ];
 
 @NgModule({
--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { FormsRoutingModule } from './forms-routing.module';
 import { TemplateFormsComponent } from './template-forms/template-forms.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 
 @NgModule({
   declarations: [
-    TemplateFormsComponent
+    TemplateFormsComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     CommonModule,
     FormsRoutingModule,
     FormsModule,
+    ReactiveFormsModule
     
   ]
 })
