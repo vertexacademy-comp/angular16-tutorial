42d54f07d56ce338df57a20147a6b58dccaaf452
Prashant <vacationlocation.devteam@gmail.com>
2025-01-30 17:07:02 +0530

    Dynamice Reactive forms

--- src/app/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/forms/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -19,18 +19,38 @@ export class ReactiveFormsComponent implements OnInit{
       email: ['', [Validators.required, Validators.email]],
       gender: ['male', Validators.required],
       message: ['TEst Message', Validators.required],
+      passengers: this.fb.array([])
     })
   }
  
 
   ngOnInit(): void {
-   
   }
+
+  get passengers() :FormArray{
+    return this.userForm.get("passengers") as FormArray
+  }
+
+
+  newPassengers():FormGroup{
+    return this.fb.group({
+      passenger_name:'',
+      passenger_age:'',
+      passenger_gender:'',
+    })
+  }
+
+addPassenger(){
+  this.passengers.push(this.newPassengers());
+}
+
+removePassenger(i:number){
+  this.passengers.removeAt(i);
+}
 
   submitForm(){
     console.log(this.userForm.value)
-    console.log(this.userForm.controls)
+    //console.log(this.userForm.controls)
     this.userForm.reset()
   }
-
 }
